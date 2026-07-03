57b0160e9a734e10a51eca6046060d37e892a56d
midwestFaust <joaquinfelizjueves@gmail.com>
2026-07-03 18:29:54 -0400

    errores menores en la clase abstracta mascota

--- Mascotas/Mascota.java
+++ Mascotas/Mascota.java
@@ -1,11 +1,11 @@
 package mascotas;
 
-public class Mascota{
+public abstract class Mascota{
     protected String especie;
     protected double precio;
     protected int nivelSalud;
 
-    public abstract class Mascota(String especie, double precio){
+    public Mascota(String especie, double precio){
         this.especie = especie;
         this.precio = precio;
         this.nivelSalud = 100; // valor inicial todos al 100
@@ -21,8 +21,8 @@ public class Mascota{
         return nivelSalud;
     }
 
-    public void setNivelSalud(int salud
+    public void setNivelSalud(int salud)
         this.nivelSalud = salud;
     }
-    public abstract void makeSound();
+    public abstract class void makeSound();
 }

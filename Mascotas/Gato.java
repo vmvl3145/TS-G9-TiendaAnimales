0f2691ea8ecc69e8a87c2945214d3ec0e8e0da69
JuaninLechugin <joanavarrete2025@inf.udec.cl>
2026-07-03 22:29:09 -0400

    Reapply "Tipeo en imports"
    This reverts commit 0fd2b3b9a87351c0ffdd80b1b1fa8c2168ae3ee4.

--- Mascotas/Gato.java
+++ Mascotas/Gato.java
@@ -1,4 +1,4 @@
-package mascotas;
+package Mascotas;
 
 public class Gato extends Mascota{
     public Gato(){
--- Mascotas/Perro.java
+++ Mascotas/Perro.java
@@ -1,4 +1,4 @@
-package mascotas;
+package Mascotas;
 
 public class Perro extends Mascota{
     public Perro(){

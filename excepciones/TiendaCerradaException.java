6e27b99f4239f4da367bb55b024b295ef7cbc8e3
JuaninLechugin <joanavarrete2025@inf.udec.cl>
2026-07-08 23:47:20 -0400

    Javadoc para TiendaCerradaException.java

--- excepciones/TiendaCerradaException.java
+++ excepciones/TiendaCerradaException.java
@@ -1,6 +1,13 @@
 package excepciones;
 
+/**
+ * Excepcion que se lanza cuando la tienda se encuentra cerrada
+ */
 public class TiendaCerradaException extends Exception {
+    /**
+     * Construye una nueva excepción con un mensaje detallado.
+     * @param mensaje El texto descriptivo del error.
+     */
     public TiendaCerradaException(String mensaje) {
         super(mensaje);
     }

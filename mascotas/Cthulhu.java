4d715784a4e36a941b1e8ec20bced04371ea4e1a
JuaninLechugin <joanavarrete2025@inf.udec.cl>
2026-07-08 22:42:43 -0400

    Javadoc para Cthulhu.java

--- mascotas/Cthulhu.java
+++ mascotas/Cthulhu.java
@@ -1,5 +1,12 @@
 package mascotas;
 
+/**
+ * Representa a la deidad cósmica Cthulhu como una mascota adquirible en la tienda.
+ * Esta clase actúa como un "Easter egg" dentro del simulador, destacando por su
+ * precio inalcanzable y su naturaleza de horror cósmico.
+ * Hereda de la clase base {@link Mascota}.
+ */
+
 public class Cthulhu extends Mascota{
     public Cthulhu(){
     super("Cthulhu", 9999.0);
